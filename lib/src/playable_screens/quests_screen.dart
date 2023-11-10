78795e9d941de83985b2a57e3ff45e2548be1952
Patryk <65673390+PyDeKa@users.noreply.github.com>
2023-11-10 11:18:12 +0100

    UI quests awards (#14)
    * temporary_database
    
    moving sample data to a folder with temporary data
    
    * temporary_database
    
    some little changes
    fluttertoast
    
    * Ui_quests_awards
    
    Created a user interface for awarding prizes.
    Added a day simulator (in the future, it will be a separate module).
    Ability to receive rewards of varying significance, i.e., the further along, the better the rewards.
    Exception handling
    
    * Ui_quest_awards

--- lib/src/playable_screens/quests_screen.dart
+++ lib/src/playable_screens/quests_screen.dart
@@ -2,6 +2,12 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:game_template/src/playable_screens/tavern_screen.dart';
 import 'package:game_template/src/temporary_database/quests/available_quests_data.dart';
+import 'package:game_template/src/temporary_database/quests/categories_data.dart';
+import 'package:game_template/src/temporary_database/quests/quest.dart';
+import 'package:game_template/src/temporary_database/quests/reward.dart';
+import 'dart:async';
+
+import 'package:game_template/src/temporary_database/quests/rewards_data.dart';
 
 class QuestsScreen extends StatefulWidget {
   @override
@@ -17,8 +23,67 @@ final List<String> categories = [
 ];
 
 class _QuestsScreenState extends State<QuestsScreen> {
-  String selectedCategory = 'Wszystkie';
+  String selectedCategory = 'All';
   int _selectedIndex = 0;
+
+  bool wheelOfFortuneAvailable = true;
+  String wheelOfFortuneResult = '';
+  String newDay = "";
+  int currentDay = 1;
+  int spinsRemaining = 7;
+  bool wheelSpinAllowed = true;
+  List<bool> daysSpun = List.generate(7, (index) => false);
+
+  Timer? _dailyResetTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    _generateRewards();
+    _startDailyResetTimer();
+  }
+
+  void _resetAndShuffleRewards() {
+    _generateRewards();
+    rewards.shuffle();
+    setState(() {
+      daysSpun = List.generate(7, (index) => false);
+    });
+  }
+
+  void _startDailyResetTimer() {
+    const oneDay = Duration(days: 1);
+    _dailyResetTimer = Timer.periodic(oneDay, (timer) {
+      if (mounted) {
+        _resetAndShuffleRewards();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _dailyResetTimer?.cancel(); // Anuluj timer przed usunięciem widgetu
+    super.dispose();
+  }
+
+  void _generateRewards() {
+    // Assign rewards based on the current day
+    for (int i = 0; i < rewards.length; i++) {
+      if (currentDay <= 3 && rewards[i].value == 1) {
+        rewards[i] = Reward(rewards[i].name, 1);
+      } else if (currentDay <= 6 && rewards[i].value == 2) {
+        rewards[i] = Reward(rewards[i].name, 2);
+      } else if (currentDay == 7 && rewards[i].value == 3) {
+        rewards[i] = Reward(rewards[i].name, 3);
+      }
+    }
+
+    // Shuffle rewards
+    rewards.shuffle();
+
+    // Sort rewards from least valuable to most valuable
+    rewards.sort((a, b) => a.value.compareTo(b.value));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,22 +91,61 @@ class _QuestsScreenState extends State<QuestsScreen> {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         actions: [
-          Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: DropdownButton<String>(
-              value: selectedCategory,
-              onChanged: (category) {
-                setState(() {
-                  selectedCategory = category!;
-                });
-              },
-              items: categories.map((String category) {
-                return DropdownMenuItem<String>(
+          PopupMenuButton<String>(
+            onSelected: (category) {
+              setState(() {
+                selectedCategory = category;
+              });
+            },
+            itemBuilder: (BuildContext context) {
+              return categories.map((String category) {
+                return PopupMenuItem<String>(
                   value: category,
                   child: Text(category),
                 );
-              }).toList(),
+              }).toList();
+            },
+            child: Container(
+              height: 20,
+              alignment: Alignment.center,
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                selectedCategory,
+                style: TextStyle(fontSize: 14.0),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  _addDay();
+                },
+                child: Text('+'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                "Day $currentDay",
+                style: TextStyle(fontSize: 18.0),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              child: IconButton(
+                icon: Icon(Icons.casino),
+                onPressed: () {
+                  _showDiceMenu();
+                },
+              ),
             ),
           ),
         ],
@@ -49,8 +153,7 @@ class _QuestsScreenState extends State<QuestsScreen> {
       body: ListView(
         children: <Widget>[
           for (var quest in availableQuests)
-            if (selectedCategory == 'Wszystkie' ||
-                quest.category == selectedCategory)
+            if (selectedCategory == 'All' || quest.category == selectedCategory)
               ListTile(
                 title: Text(quest.title),
                 subtitle: Text(quest.description),
@@ -68,7 +171,7 @@ class _QuestsScreenState extends State<QuestsScreen> {
                       IconButton(
                         icon: Icon(Icons.check),
                         onPressed: () {
-                          _showRewardDialog(context);
+                          _showRewardDialogQuest(context);
                         },
                       ),
                   ],
@@ -78,10 +181,8 @@ class _QuestsScreenState extends State<QuestsScreen> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.black,
-        unselectedItemColor:
-            Colors.white, // Kolor ikon i etykiet dla niezaznaczonych pozycji
-        selectedItemColor:
-            Colors.white, // Kolor ikon i etykiet dla zaznaczonej pozycji
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Colors.white,
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.arrow_back),
@@ -115,56 +216,182 @@ class _QuestsScreenState extends State<QuestsScreen> {
       ),
     );
   }
-}
 
-void _showInfoDialog(BuildContext context, Quest quest) {
-  showDialog(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: Text('Informacje o misji').tr(),
-        content: Text(quest.details).tr(),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('OK').tr(),
+  String _addDay() {
+    setState(() {
+      currentDay = (currentDay % 7) + 1;
+      newDay = currentDay.toString();
+    });
+    return newDay;
+  }
+
+  void _spinWheelOfFortune() {
+    if (!daysSpun[currentDay - 1]) {
+      final reward = rewards[currentDay - 1];
+
+      if (currentDay > 1 && !daysSpun[currentDay - 2]) {
+        _showSkippedDayDialog();
+        _resetAndShuffleRewards();
+        _resetDayCounter();
+      } else {
+        if (!daysSpun[currentDay - 1]) {
+          _showRewardDialog(context, reward);
+          daysSpun[currentDay - 1] = true;
+
+          // Sprawdź, czy wszystkie nagrody zostały już odebrane
+          if (daysSpun.every((day) => day)) {
+            _resetDayCounter();
+            _resetAndShuffleRewards();
+          }
+        } else {
+          _showAlreadyClaimedDialog();
+        }
+      }
+    } else {
+      _showAlreadyClaimedDialog();
+    }
+  }
+
+  void _showAlreadyClaimedDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Warning').tr(),
+          content: Text('Reward for this day has already been claimed.').tr(),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK').tr(),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showSkippedDayDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Warning').tr(),
+          content: Text('Skipped a day. Returning to day one.').tr(),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _resetDayCounter();
+              },
+              child: Text('OK').tr(),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _resetDayCounter() {
+    setState(() {
+      currentDay = 1;
+    });
+    _resetAndShuffleRewards();
+  }
+
+  void _showInfoDialog(BuildContext context, Quest quest) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Mission Information').tr(),
+          content: Text(quest.details).tr(),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK').tr(),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showRewardDialog(BuildContext context, Reward reward) async {
+    Navigator.of(context).pop();
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Claim Reward').tr(),
+          content: Text('Congratulations! You receive ${reward.name}.').tr(),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _showDiceMenu();
+              },
+              child: Text('OK').tr(),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showRewardDialogQuest(BuildContext context) async {
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Claim Reward').tr(),
+          content: Text('Congratulations!').tr(),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK').tr(),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showDiceMenu() async {
+    await showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ListTile(
+                  title: Text('Get reward'),
+                  onTap: () {
+                    _spinWheelOfFortune();
+                  },
+                ),
+                Divider(),
+                Text('Last 7 days:'),
+                for (int i = 0; i < 7; i++)
+                  ListTile(
+                    title: Text('Day ${i + 1}'),
+                    trailing: daysSpun[i]
+                        ? Icon(Icons.check_circle, color: Colors.green)
+                        : Icon(Icons.cancel, color: Colors.red),
+                  ),
+              ],
+            ),
           ),
-        ],
-      );
-    },
-  );
-}
-
-void _showRewardDialog(BuildContext context) {
-  showDialog(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: Text('Odbierz nagrodę').tr(),
-        content: Text('Gratulacje! Odbierasz nagrodę.').tr(),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('OK').tr(),
-          ),
-        ],
-      );
-    },
-  );
-}
-
-class Quest {
-  final String title;
-  final String description;
-  final String category;
-  final String details;
-  bool isInfoAvailable;
-  bool isCompleted;
-
-  Quest(this.title, this.description, this.category, this.details,
-      {this.isInfoAvailable = false, this.isCompleted = false});
+        );
+      },
+    );
+  }
 }
---
+++ lib/src/temporary_database/quests/rewards_data.dart
@@ -0,0 +1,14 @@
+import 'package:game_template/src/playable_screens/quests_screen.dart';
+import 'package:game_template/src/temporary_database/quests/reward.dart';
+
+List<Reward> rewards = [
+  Reward("Easy Item 1", 1),
+  Reward("Easy Item 2", 1),
+  Reward("Easy Item 3", 1),
+  Reward("Medium Item 4", 2),
+  Reward("Medium Item 5", 2),
+  Reward("Medium Item 6", 2),
+  Reward("Hard Item 7", 3),
+  Reward("Premium Currency 8", 3),
+  Reward("Premium Currency 9", 3),
+];
