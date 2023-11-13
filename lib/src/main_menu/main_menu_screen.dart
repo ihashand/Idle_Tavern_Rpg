099e63d345dc3f542e8d8fc9cddfd4a1faa086fb
Damian Krzemien <damian.krzem@gmail.com>
2023-11-13 10:40:07 +0100

    feat(menu): dynamic image in background (#18)

--- lib/src/main_menu/main_menu_screen.dart
+++ lib/src/main_menu/main_menu_screen.dart
@@ -1,7 +1,3 @@
-// Copyright 2022, the Flutter project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -14,7 +10,7 @@ import '../settings/settings.dart';
 import '../style/responsive_screen.dart';
 
 class MainMenuScreen extends StatelessWidget {
-  const MainMenuScreen({super.key});
+  const MainMenuScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -22,112 +18,127 @@ class MainMenuScreen extends StatelessWidget {
     final settingsController = context.watch<SettingsController>();
     final audioController = context.watch<AudioController>();
 
-    return ValueListenableBuilder<String>(
-        valueListenable: settingsController.playerName,
-        builder: (context, playerName, child) {
-          return Scaffold(
-            body: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/images/menu/tawerna.png"),
-                  fit: BoxFit.fill,
-                ),
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final squarishMainAreaHeight = screenHeight * 0.5;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/menu/tawerna.png"),
+                fit: BoxFit.fill,
               ),
-              child: ResponsiveScreen(
-                mainAreaProminence: 0.45,
-                squarishMainArea: Center(
-                  child: Transform.rotate(
-                    angle: -0.05,
-                    child: SizedBox(
-                      height: 800,
-                      child: Text(
-                        playerName,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontFamily: 'Permanent Marker',
-                          fontSize: 40,
-                          height: 1,
-                        ),
+            ),
+            child: ResponsiveScreen(
+              mainAreaProminence: 0.45,
+              squarishMainArea: Center(
+                child: Transform.rotate(
+                  angle: -0.05,
+                  child: SizedBox(
+                    height: squarishMainAreaHeight,
+                    child: Text(
+                      settingsController.playerName.value,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Permanent Marker',
+                        fontSize: screenWidth * 0.05,
+                        height: 1,
                       ),
                     ),
                   ),
                 ),
-                rectangularMenuArea: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    FilledButton(
-                      onPressed: () {
-                        audioController.playSfx(SfxType.buttonTap);
-                        GoRouter.of(context).go('/play');
-                      },
-                      child: Text('play').tr(),
+              ),
+              rectangularMenuArea: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  FilledButton(
+                    onPressed: () {
+                      audioController.playSfx(SfxType.buttonTap);
+                      GoRouter.of(context).go('/play');
+                    },
+                    child: Text('play').tr(),
+                  ),
+                  _gap,
+                  if (gamesServicesController != null) ...[
+                    _hideUntilReady(
+                      ready: gamesServicesController.signedIn,
+                      child: FilledButton(
+                        onPressed: () =>
+                            gamesServicesController.showAchievements(),
+                        child: const Text('Achievements'),
+                      ),
                     ),
                     _gap,
-                    if (gamesServicesController != null) ...[
-                      _hideUntilReady(
-                        ready: gamesServicesController.signedIn,
-                        child: FilledButton(
-                          onPressed: () =>
-                              gamesServicesController.showAchievements(),
-                          child: const Text('Achievements'),
-                        ),
+                    _hideUntilReady(
+                      ready: gamesServicesController.signedIn,
+                      child: FilledButton(
+                        onPressed: () =>
+                            gamesServicesController.showLeaderboard(),
+                        child: const Text('Leaderboard'),
                       ),
-                      _gap,
-                      _hideUntilReady(
-                        ready: gamesServicesController.signedIn,
-                        child: FilledButton(
-                          onPressed: () =>
-                              gamesServicesController.showLeaderboard(),
-                          child: const Text('Leaderboard'),
-                        ),
-                      ),
-                      _gap,
-                    ],
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        SizedBox(width: 1), // Odstęp między przyciskami
-                        ValueListenableBuilder<bool>(
-                          valueListenable: settingsController.muted,
-                          builder: (context, muted, child) {
-                            return IconButton(
-                              onPressed: () => settingsController.toggleMuted(),
-                              icon: Icon(
-                                  muted ? Icons.volume_off : Icons.volume_up),
-                              color: Color.fromARGB(255, 6, 27, 44),
-                            );
-                          },
-                        ),
-                        IconButton(
-                          onPressed: () =>
-                              GoRouter.of(context).push('/settings'),
-                          icon: Icon(
-                            Icons.settings,
-                            color: Color.fromARGB(255, 6, 27, 44),
-                          ),
-                        ),
-                      ],
                     ),
+                    _gap,
                   ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SizedBox(width: screenWidth * 0.01),
+                      ValueListenableBuilder<bool>(
+                        valueListenable: settingsController.muted,
+                        builder: (context, muted, child) {
+                          return IconButton(
+                            onPressed: () => settingsController.toggleMuted(),
+                            icon: Icon(
+                                muted ? Icons.volume_off : Icons.volume_up),
+                            color: Color.fromARGB(255, 6, 27, 44),
+                          );
+                        },
+                      ),
+                      IconButton(
+                        onPressed: () => GoRouter.of(context).push('/settings'),
+                        icon: Icon(
+                          Icons.settings,
+                          color: Color.fromARGB(255, 6, 27, 44),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: -screenHeight * 0.03,
+            right: screenWidth * 0.03,
+            child: GestureDetector(
+              onTap: () {
+                GoRouter.of(context).push('/interior');
+              },
+              child: Container(
+                padding: EdgeInsets.all(0),
+                margin: EdgeInsets.all(0),
+                width: screenWidth * 0.6,
+                height: screenHeight * 0.6,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/menu/karczma.png'),
+                  ),
                 ),
               ),
             ),
-          );
-        });
+          ),
+        ],
+      ),
+    );
   }
 
-  /// Prevents the game from showing game-services-related menu items
-  /// until we're sure the player is signed in.
-  ///
-  /// This normally happens immediately after game start, so players will not
-  /// see any flash. The exception is folks who decline to use Game Center
-  /// or Google Play Game Services, or who haven't yet set it up.
   Widget _hideUntilReady({required Widget child, required Future<bool> ready}) {
     return FutureBuilder<bool>(
       future: ready,
       builder: (context, snapshot) {
-        // Use Visibility here so that we have the space for the buttons
-        // ready.
         return Visibility(
           visible: snapshot.data ?? false,
           maintainState: true,
