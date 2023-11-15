bb394eb982b5c2f787845b829496930cca3b9ffe
Patryk <65673390+PyDeKa@users.noreply.github.com>
2023-11-15 18:34:53 +0100

    Quests (#21)
    * Quests
    
    Provider for days
    Provider for daySpun
    
    * fix
    
    * fix

--- lib/GameState.dart
+++ lib/GameState.dart
@@ -3,9 +3,59 @@ import 'package:game_template/src/temporary_database/tavern/tavern_models/item.d
 
 class GameState extends ChangeNotifier {
   List<Item> inventory = [];
+  int _currentDay = 1;
+  String _wheelOfFortuneResult = ''; // Dodane pole dla wyniku koła fortuny
+  List<bool> _daysSpun = List.generate(
+      7, (index) => false); // Inicjalizacja stanu odebranych nagród
+
+  int get currentDay => _currentDay;
+  String get wheelOfFortuneResult =>
+      _wheelOfFortuneResult; // Getter dla wyniku koła fortuny
+  List<bool> get daysSpun => _daysSpun;
 
   void addItemToInventory(Item item) {
     inventory.add(item);
+    notifyListeners();
+  }
+
+  set currentDay(int day) {
+    _currentDay = day;
+    notifyListeners(); // Powiadamianie o zmianie wartości currentDay
+  }
+
+  // Setter dla wyniku koła fortuny
+  set wheelOfFortuneResult(String result) {
+    _wheelOfFortuneResult = result;
+    notifyListeners();
+  }
+
+  void spinWheelOfFortune(String result) {
+    _wheelOfFortuneResult = result;
+    notifyListeners();
+  }
+
+  void resetWheelOfFortuneResult() {
+    _wheelOfFortuneResult = '';
+    notifyListeners();
+  }
+
+  void incrementDay() {
+    if (_currentDay < 7) {
+      _currentDay++;
+    } else {
+      _currentDay = 1; // Wraca do pierwszego dnia po osiągnięciu 7 dni
+    }
+    notifyListeners(); // Powiadamianie o zmianie wartości currentDay
+  }
+
+  set daysSpun(List<bool> value) {
+    _daysSpun = value;
+    notifyListeners();
+  }
+
+  // Metoda do aktualizacji stanu odebranych nagród
+  void updateDaysSpun(List<bool> newDaysSpun) {
+    _daysSpun = newDaysSpun;
     notifyListeners();
   }
 }
