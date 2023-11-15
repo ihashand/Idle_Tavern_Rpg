0270c9f50c10fb9189369305455c9a73c34fffe7
Damian Krzemien <damian.krzem@gmail.com>
2023-11-15 20:37:30 +0100

    emmployees

--- lib/src/temporary_database/tavern/tavern_data/employees_data.dart
+++ lib/src/temporary_database/tavern/tavern_data/employees_data.dart
@@ -1,3 +1,108 @@
 import 'package:game_template/src/temporary_database/tavern/tavern_models/employee.dart';
 
-List<Employee> employees = [];
+List<Employee> employees = [
+  Employee(
+    name: 'Ivan Redbeard',
+    category: EmployeeCategory.Waiter,
+    skill: 3,
+    payment: 50.0,
+    iconUrl: 'assets/images/employees/ivan.jpeg',
+  ),
+  Employee(
+    name: 'Adam Longfoot',
+    category: EmployeeCategory.Cook,
+    skill: 4,
+    payment: 70.0,
+    iconUrl: 'assets/images/employees/adam.jpeg',
+  ),
+  Employee(
+      name: 'Robert Swiftserver',
+      category: EmployeeCategory.Waiter,
+      skill: 5,
+      payment: 80.0,
+      iconUrl: 'assets/images/employees/robert.jpeg'),
+  Employee(
+      name: 'Alice Axewielder',
+      category: EmployeeCategory.Lumberjack,
+      skill: 3,
+      payment: 60.0,
+      iconUrl: 'assets/images/employees/alice.jpeg'),
+  Employee(
+      name: 'David Timbermaster',
+      category: EmployeeCategory.Lumberjack,
+      skill: 2,
+      payment: 45.0,
+      iconUrl: 'assets/images/employees/david.jpeg'),
+  Employee(
+      name: 'Emily Dustsweeper',
+      category: EmployeeCategory.Cleaner,
+      skill: 1,
+      payment: 30.0,
+      iconUrl: 'assets/images/employees/emily.jpeg'),
+  Employee(
+      name: 'Michael Spicemaster',
+      category: EmployeeCategory.Cook,
+      skill: 3,
+      payment: 60.0,
+      iconUrl: 'assets/images/employees/michael.jpeg'),
+  Employee(
+      name: 'Sophia Smilesling',
+      category: EmployeeCategory.Waiter,
+      skill: 4,
+      payment: 65.0,
+      iconUrl: 'assets/images/employees/sophia.jpeg'),
+  Employee(
+      name: 'William Logcrusher',
+      category: EmployeeCategory.Lumberjack,
+      skill: 3,
+      payment: 55.0,
+      iconUrl: 'assets/images/employees/william.jpeg'),
+  Employee(
+      name: 'Olivia Broomwhisperer',
+      category: EmployeeCategory.Cleaner,
+      skill: 2,
+      payment: 35.0,
+      iconUrl: 'assets/images/employees/olivia.jpeg'),
+  Employee(
+      name: 'Daniel Jokewaiter',
+      category: EmployeeCategory.Waiter,
+      skill: 3,
+      payment: 55.0,
+      iconUrl: 'assets/images/employees/daniel.jpeg'),
+  Employee(
+      name: 'Ava Flamechef',
+      category: EmployeeCategory.Cook,
+      skill: 5,
+      payment: 75.0,
+      iconUrl: 'assets/images/employees/ava.jpeg'),
+  Employee(
+      name: 'Ethan Forestmaster',
+      category: EmployeeCategory.Lumberjack,
+      skill: 4,
+      payment: 65.0,
+      iconUrl: 'assets/images/employees/ethan.jpeg'),
+  Employee(
+      name: 'Helga Dustdancer',
+      category: EmployeeCategory.Cleaner,
+      skill: 1,
+      payment: 25.0,
+      iconUrl: 'assets/images/employees/helga.jpeg'),
+  Employee(
+      name: 'Benjamin Grinservant',
+      category: EmployeeCategory.Waiter,
+      skill: 4,
+      payment: 70.0,
+      iconUrl: 'assets/images/employees/benjamin.jpeg'),
+  Employee(
+      name: 'Mia Spicecharmer',
+      category: EmployeeCategory.Cook,
+      skill: 2,
+      payment: 55.0,
+      iconUrl: 'assets/images/employees/mia.jpeg'),
+  Employee(
+      name: 'Gertruda Breadmkaer',
+      category: EmployeeCategory.Cook,
+      skill: 5,
+      payment: 70.0,
+      iconUrl: 'assets/images/employees/gertruda.jpeg'),
+];
