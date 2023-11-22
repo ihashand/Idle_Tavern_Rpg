2a502163ad003de43d8e40cde583837829b71d2b
Damian Krzemien <damian.krzem@gmail.com>
2023-11-22 09:09:30 +0100

    Feat/prestige (#26)
    * profile + prestige
    
    * update
    
    * Update profile_screen.dart
    
    * Update race.dart
    
    * updates for profile

--- lib/src/temporary_database/profile/data/race.dart
+++ lib/src/temporary_database/profile/data/race.dart
@@ -2,12 +2,12 @@ import 'package:game_template/constants/fantasy_race.dart';
 import 'package:game_template/src/temporary_database/profile/models/race.dart';
 
 final List<Race> fantasyRaces = [
-  Race(FantasyRace.Human, 307, "assets/images/race/vampire.png"),
-  Race(FantasyRace.Dwarf, 49, "assets/images/race/vampire.png"),
-  Race(FantasyRace.Goblin, 75, "assets/images/race/vampire.png"),
-  Race(FantasyRace.Orc, 125, "assets/images/race/vampire.png"),
-  Race(FantasyRace.Elf, 80, "assets/images/race/vampire.png"),
-  Race(FantasyRace.Centaur, 117, "assets/images/race/vampire.png"),
+  Race(FantasyRace.Human, 307, "assets/images/race/human.png"),
+  Race(FantasyRace.Dwarf, 49, "assets/images/race/dwarf.png"),
+  Race(FantasyRace.Goblin, 75, "assets/images/race/goblin.png"),
+  Race(FantasyRace.Orc, 125, "assets/images/race/orc.png"),
+  Race(FantasyRace.Elv, 80, "assets/images/race/elv.png"),
+  Race(FantasyRace.Centaur, 117, "assets/images/race/centaur.png"),
   Race(FantasyRace.Werewolf, 90, "assets/images/race/werewolf.png"),
   Race(FantasyRace.Vampire, 253, "assets/images/race/vampire.png"),
 ];
